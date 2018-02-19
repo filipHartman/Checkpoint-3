781482d7ce3c2daa7d64cc3c8ec30bca03723c71
filipHartman <filip.hartman23@gmail.com>
2018-02-19 10:16:54 +0100

    Add string representation of book publisher

--- src/model/BookPublisher.java
+++ src/model/BookPublisher.java
@@ -1,4 +1,53 @@
 package model;
 
 public class BookPublisher {
+    private String publsherId;
+    private String name;
+    private String city;
+    private String country;
+
+    public BookPublisher(String publsherId, String name, String city, String country) {
+        this.publsherId = publsherId;
+        this.name = name;
+        this.city = city;
+        this.country = country;
+    }
+
+    public String getPublsherId() {
+        return publsherId;
+    }
+
+    public void setPublsherId(String publsherId) {
+        this.publsherId = publsherId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher: id "+publsherId+ ", " + name + ", "
+                + city + ", "+ country;
+    }
 }
