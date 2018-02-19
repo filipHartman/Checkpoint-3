9704ad8ff949ae14c4e7e5c54f6040c4d4ebd2bf
filipHartman <filip.hartman23@gmail.com>
2018-02-19 11:18:01 +0100

    Add importing author by given id

--- src/dao/DaoAuthor.java
+++ src/dao/DaoAuthor.java
@@ -2,6 +2,10 @@ package dao;
 
 import model.Author;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DaoAuthor {
     public Author createAuthor(int authorId, String name, String surname, int birthYear, String city, String country) {
         return new Author(
@@ -10,6 +14,26 @@ public class DaoAuthor {
 
     }
 
-    
-
+    public Author importAuthor(int authorId) {
+        Author author = null;
+        PreparedStatement ps = null;
+        String query = "SELECT * FROM Authors WHERE author_id = ?";
+        try {
+            ps = DBConnection.getConnection().prepareStatement(query);
+            ps.setInt(1, authorId);
+            ResultSet rs = ps.executeQuery();
+            if (!rs.isClosed()) {
+                String name = rs.getString("name");
+                String surname = rs.getString("surname");
+                int birthYear = rs.getInt("birth_year");
+                String city = rs.getString("city");
+                String country = rs.getString("country");
+                author = createAuthor(authorId, name, surname, birthYear, city, country);
+                rs.close();
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+        return author;
+    }
 }
