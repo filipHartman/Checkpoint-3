b2a1b71e551b1ff8031f4f356dbb02e4264d534e
filipHartman <filip.hartman23@gmail.com>
2018-02-19 11:23:07 +0100

    Add importing all authors

--- src/dao/DaoAuthor.java
+++ src/dao/DaoAuthor.java
@@ -5,6 +5,7 @@ import model.Author;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class DaoAuthor {
     public Author createAuthor(int authorId, String name, String surname, int birthYear, String city, String country) {
@@ -35,5 +36,30 @@ public class DaoAuthor {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
         }
         return author;
+    }
+
+    public ArrayList<Author> importAllAuthors() {
+        ArrayList<Author> authors = new ArrayList<>();
+        PreparedStatement ps = null;
+        String query = "SELECT * FROM Authors";
+        try {
+            ps = DBConnection.getConnection().prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                int authorId = rs.getInt("author_id");
+                String name = rs.getString("name");
+                String surname = rs.getString("surname");
+                int birthYear = rs.getInt("birth_year");
+                String city = rs.getString("city");
+                String country = rs.getString("country");
+                Author author = createAuthor(authorId, name, surname, birthYear, city, country);
+                authors.add(author);
+            }
+            rs.close();
+            ps.close();
+        } catch (SQLException | ClassNotFoundException e) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        }
+        return authors;
     }
 }
