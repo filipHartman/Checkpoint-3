4f16ddd8edc8686b8645882e81c88003d7f672a3
filipHartman <filip.hartman23@gmail.com>
2018-02-19 11:34:03 +0100

    Add importing book publisher by id

--- src/dao/DaoBookPublisher.java
+++ src/dao/DaoBookPublisher.java
@@ -2,10 +2,33 @@ package dao;
 
 import model.BookPublisher;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DaoBookPublisher {
     public BookPublisher createBookPublisher(String publisherId, String name, String city, String country) {
         return new BookPublisher(publisherId, name, city, country);
     }
 
-    
+    public BookPublisher importBookPublisher(String publisherId) {
+        BookPublisher bookPublisher = null;
+        PreparedStatement ps = null;
+        String query = "SELECT * FROM Publishers WHERE publisher_id = ?";
+        try {
+            ps = DBConnection.getConnection().prepareStatement(query);
+            ps.setString(1, publisherId);
+            ResultSet rs = ps.executeQuery();
+            if (!rs.isClosed()) {
+                String name = rs.getString("name");
+                String city = rs.getString("city");
+                String country = rs.getString("country");
+                bookPublisher = createBookPublisher(publisherId, name, city, country);
+                rs.close();
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+        return bookPublisher;
+    }
 }
