8c54db2a77d444b437e0dffd307b49f66ee67b55
filipHartman <filip.hartman23@gmail.com>
2018-02-19 10:05:44 +0100

    Ad setters and getters for book class

---
+++ src/model/Book.java
@@ -0,0 +1,78 @@
+package model;
+
+
+public class Book {
+    private float ISBN;
+    private String author;
+    private String title;
+    private BookPublisher publisher;
+    private int publication_year;
+    private float price;
+    private BookType bookType;
+
+    public Book(float ISBN, String author, String title, BookPublisher publisher, int publication_year, float price, BookType bookType) {
+        this.ISBN = ISBN;
+        this.author = author;
+        this.title = title;
+        this.publisher = publisher;
+        this.publication_year = publication_year;
+        this.price = price;
+        this.bookType = bookType;
+    }
+
+    public float getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(float ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public BookPublisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(BookPublisher publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getPublication_year() {
+        return publication_year;
+    }
+
+    public void setPublication_year(int publication_year) {
+        this.publication_year = publication_year;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public BookType getBookType() {
+        return bookType;
+    }
+
+    public void setBookType(BookType bookType) {
+        this.bookType = bookType;
+    }
+}
