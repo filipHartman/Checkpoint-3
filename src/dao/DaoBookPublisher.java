562e5db241f4be99fcce7a27e94c51da5253d6d4
filipHartman <filip.hartman23@gmail.com>
2018-02-19 11:54:40 +0100

    Add importing all book publishers

--- src/dao/DaoBookPublisher.java
+++ src/dao/DaoBookPublisher.java
@@ -5,6 +5,7 @@ import model.BookPublisher;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class DaoBookPublisher {
     public BookPublisher createBookPublisher(String publisherId, String name, String city, String country) {
@@ -30,5 +31,26 @@ public class DaoBookPublisher {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
         }
         return bookPublisher;
+    }
+
+    public ArrayList<BookPublisher> importAllBookPublishers() {
+        ArrayList<BookPublisher> bookPublishers = new ArrayList<>();
+        PreparedStatement ps = null;
+        String query = "SELECT * FROM Publishers";
+        try {
+            ps = DBConnection.getConnection().prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                String publisherId = rs.getString("publisher_id");
+                String name = rs.getString("name");
+                String city = rs.getString("city");
+                String country = rs.getString("country");
+                BookPublisher bookPublisher = createBookPublisher(publisherId, name, city, country);
+                bookPublishers.add(bookPublisher);
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+        }
+        return bookPublishers;
     }
 }
